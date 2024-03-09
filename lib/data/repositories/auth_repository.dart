e4beacf317b135c54e03571cce3a55902c8c4d8a
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-09 23:20:18 +0500

    update auth repository

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,24 +1,14 @@
-import 'dart:convert';
-
+import 'package:bloc_state_management_tutorial/networking.dart';
 import 'package:bloc_state_management_tutorial/utils/urls.dart';
-import 'package:http/http.dart' as http;
-
 import '../models/login_model.dart';
 
 class AuthRepository {
   Future<LoginModel?> login(String email, String password) async {
-    final http.Response response = await http.post(
-      Uri.parse(loginAPIUrl),
+    NetworkHelper networkHelper = NetworkHelper(loginAPIUrl);
+    final loginJsonResponse = await networkHelper.postData(
       body: {"email": email, "password": password},
     );
-    print(response.body);
-    if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
-      LoginModel loginModel = LoginModel.fromJson(data);
-      return loginModel;
-    }
-
-    return null;
+    LoginModel loginModel = LoginModel.fromJson(loginJsonResponse);
+    return loginModel;
   }
 }
-
